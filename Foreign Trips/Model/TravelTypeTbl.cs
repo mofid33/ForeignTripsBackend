97c47c005ae88ec02ac5f80fb948e3c6ba09eb2a
s.hendouabadi <sara.hendouabadi@gmail.com>
2023-08-30 13:53:37 +0430

    ss

--- Foreign Trips/Model/TravelTypeTbl.cs
+++ Foreign Trips/Model/TravelTypeTbl.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 namespace Foreign_Trips.Model;
 
 public partial class TravelTypeTbl
+
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
