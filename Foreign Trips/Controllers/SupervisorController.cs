3a6da367fea4992c7f1513d06032552dc6b87069
amir333 <a.mofid76@yahoo.com>
2023-11-05 16:25:47 +0330

    ss

--- Foreign Trips/Controllers/SupervisorController.cs
+++ Foreign Trips/Controllers/SupervisorController.cs
@@ -107,15 +107,10 @@ namespace Foreign_Trips.Controllers
 
         [HttpGet]
         [Route("GetAgents")]
-<<<<<<< HEAD
-        public async Task<ActionResult<IEnumerable<AgentTbl>>> GetAgents([FromQuery(Name = "page")] int page, [FromQuery(Name = "pageSize")] int pageSize)
-        {
-            var Agents = await _agentRepository.GetAgent(page == 0 ? 1 : page, pageSize == 0 ? 10 : pageSize);
-=======
+
         public async Task<ActionResult<IEnumerable<AgentTbl>>> GetAgents([FromQuery(Name = "page")] int page, [FromQuery(Name = "pageSize")] int pageSize, String search)
         {
             var Agents = await _agentRepository.GetAgent(page == 0 ? 1 : page, pageSize == 0 ? 10 : pageSize, search);
->>>>>>> 52a58f6295f6e8d3bf78cb21e0117d2231ae27ed
 
             return Ok(
                 //_mapper.Map<IEnumerable<AgentTbl>>(Agents)
